c5d6ab0bb3b25005cfeb030d0e923be3b70e225a
MNIX <mvppl123@gmail.com>
2022-08-14 19:31:08 +0200

    jakarta

---
+++ mvc/src/main/java/com/example/mvc/MyWebAppInitializer.java
@@ -0,0 +1,10 @@
+package com.example.mvc;
+
+//
+//public class MyWebAppInitializer implements WebApplicationInitializer {
+//
+//    @Override
+//    public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
+//
+//    }
+//}
