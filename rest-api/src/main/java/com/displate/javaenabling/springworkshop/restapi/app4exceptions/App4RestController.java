a05ded4b127fb6db90a1638999ed6a37e4fc77a5
dominik.otkinski <dominik.otkinski@displate.com>
2022-08-16 14:07:29 +0200

    Solve rest-api-4

--- rest-api/src/main/java/com/displate/javaenabling/springworkshop/restapi/app4exceptions/App4RestController.java
+++ rest-api/src/main/java/com/displate/javaenabling/springworkshop/restapi/app4exceptions/App4RestController.java
@@ -1,7 +1,14 @@
 package com.displate.javaenabling.springworkshop.restapi.app4exceptions;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestController
 class App4RestController {
@@ -9,5 +16,16 @@ class App4RestController {
     @GetMapping("/bad-request")
     String getBadRequest() {
         throw new RuntimeException("Something wrong");
+    }
+}
+
+@ControllerAdvice
+class MyExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(
+            value = {RuntimeException.class}
+    )
+    ResponseEntity<Object> handleRuntimeException(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, ex.getMessage(), HttpHeaders.EMPTY, HttpStatus.BAD_REQUEST, request);
     }
 }
