851f48fde06e40abb2da843da7e0c5a0a7376bf0
dominik.otkinski <dominik.otkinski@displate.com>
2022-08-17 10:34:22 +0200

    example with profiles

--- context/src/main/java/com/displate/javaenabling/springworkshop/context/app2configuration/MyConfiguration.java
+++ context/src/main/java/com/displate/javaenabling/springworkshop/context/app2configuration/MyConfiguration.java
@@ -2,11 +2,13 @@ package com.displate.javaenabling.springworkshop.context.app2configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 class MyConfiguration {
 
     @Bean
+    @Profile("integration")
     UserRepository userRepository() {
         return new UserRepository();
     }
--- context/src/test/java/com/displate/javaenabling/springworkshop/context/app2configuration/ConfigurationTest.java
+++ context/src/test/java/com/displate/javaenabling/springworkshop/context/app2configuration/ConfigurationTest.java
@@ -3,10 +3,12 @@ package com.displate.javaenabling.springworkshop.context.app2configuration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@ActiveProfiles({"integration"})
 class ConfigurationTest {
 
     public static final User DRAKE = new User("Drake", "Canada");
